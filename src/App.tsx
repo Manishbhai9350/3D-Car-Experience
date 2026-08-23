6db75a35c76808fc961f756a0bef72779042ac29
Manishbhai9350 <keshraram294@gmail.com>
2026-08-23 20:13:18 +0530

    Update overlay height to 50% and add progress bar component for loading indication

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Canvas } from "@react-three/fiber";
 import "./App.css";
-import { Stats } from "@react-three/drei";
+import { Progress, Stats, useProgress } from "@react-three/drei";
 import Lights from "./components/lights";
 import Tunnel from "./components/tunnel";
 import UI from "./components/ui";
@@ -19,9 +19,9 @@ import {
 } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-import CameraMovement from "./components/camera/CameraMove";
-import Car from "./components/car";
 import useWindow from "./hooks/useWindow";
+import Car from "./components/car";
+import CameraMovement from "./components/camera/CameraMove";
 
 // position: [2, 3, 5]
 // [0, 2, 10]
@@ -29,19 +29,24 @@ import useWindow from "./hooks/useWindow";
 const App = () => {
   const { analyser, dom } = BGM();
 
+  const LoadingTimeoutRef = useRef(0);
+
   const [Overlay, setOverlay] = useState(true);
+  const [Loaded, setLoaded] = useState(false);
+
+  const { progress } = useProgress();
 
   useEffect(() => {
-    gsap.to(".overlay", {
-      delay: 0,
-      scaleY: 0,
-      onComplete() {
+    if (progress == 100) {
+      clearTimeout(LoadingTimeoutRef.current);
+      LoadingTimeoutRef.current = setTimeout(() => {
+        setLoaded(true);
         setOverlay(false);
-      },
-    });
+      }, 500);
+    }
 
     return () => {};
-  }, []);
+  }, [progress]);
 
   // const { bokehScale, focalLength, focusDistance, height } = useControls({
   //   focusDistance: {
@@ -70,10 +75,15 @@ const App = () => {
     <>
       <Leva hidden />
       {dom}
-      <main>
-        <OverlayComponent overlay={Overlay} setOverlay={setOverlay} />
-        <UI visible={!Overlay} />
-        {/* <Leva
+      <OverlayComponent
+        loaded={Loaded}
+        overlay={Overlay}
+        setOverlay={setOverlay}
+      />
+      {true && (
+        <main>
+          <UI visible={!Overlay} />
+          {/* <Leva
           titleBar={{
             position: {
               x: -100,
@@ -81,17 +91,17 @@ const App = () => {
             },
           }}
           /> */}
-        <Canvas camera={{ fov: 50, position: [2, 3, 5] }}>
-          <AudioDriver analyser={analyser} />
-          <Stats />
-          <CameraMovement overlay={Overlay} />
-          <Floor />
-          <Lights />
-          <Cubes />
-          <Car />
-          <Tunnel audioAnalyser={analyser} />
+          <Canvas camera={{ fov: 50, position: [2, 3, 5] }}>
+            <AudioDriver analyser={analyser} />
+            <Stats />
+            <CameraMovement overlay={Overlay} />
+            <Floor />
+            <Lights />
+            <Cubes />
+            <Car />
+            <Tunnel audioAnalyser={analyser} />
 
-          {/* <EffectComposer>
+            {/* <EffectComposer>
             <DepthOfField
               focusDistance={focusDistance} // normalized — 0 = camera near, focus on car
               focalLength={focalLength} // shorter = tighter focus range
@@ -99,28 +109,32 @@ const App = () => {
               height={height} // resolution — lower = softer/cheaper bokeh
             />
           </EffectComposer> */}
-        </Canvas>
-      </main>
+          </Canvas>
+        </main>
+      )}
     </>
   );
 };
 
 const OverlayComponent = ({
+  loaded = false,
   overlay = false,
   setOverlay = () => {},
 }: {
+  loaded: boolean;
   overlay: boolean;
   setOverlay: Dispatch<SetStateAction<boolean>>;
 }) => {
   const TopRef = useRef(null);
   const BottomRef = useRef(null);
   const TimeOutID = useRef(0);
+  const BarRef = useRef<(HTMLDivElement | null)[]>([]);
 
   const { size } = useWindow();
 
   const OverlayHeight = useMemo(
-    () => (size.width < 900 ? 40 : 50),
-    [size.width],
+    () => (!loaded ? innerHeight / 2 : size.width < 900 ? 40 : 50),
+    [size.width, loaded],
   );
 
   useGSAP(() => {
@@ -128,15 +142,15 @@ const OverlayComponent = ({
 
     const OnMouseDown = () => {
       clearTimeout(TimeOutID.current);
-      document.body.style.cursor = "grabbing";
 
+      document.body.style.cursor = "grabbing";
       TimeOutID.current = setTimeout(() => {
         setOverlay(true);
       }, 200);
     };
     const OnMouseUP = () => {
-      clearTimeout(TimeOutID.current);
       document.body.style.cursor = "grab";
+      clearTimeout(TimeOutID.current);
       setOverlay(false);
     };
 
@@ -147,26 +161,67 @@ const OverlayComponent = ({
       window.removeEventListener("mousedown", OnMouseDown);
       window.removeEventListener("mouseup", OnMouseUP);
     };
-  }, [overlay]);
+  }, [overlay, loaded]);
 
   useEffect(() => {
     if (overlay) {
       gsap.to([TopRef.current, BottomRef.current], {
         height: OverlayHeight,
-        scaleY: 1,
       });
     } else {
       gsap.to([TopRef.current, BottomRef.current], {
-        height: OverlayHeight,
-        scaleY: 0,
+        height: 0,
       });
     }
 
     return () => {};
   }, [overlay, OverlayHeight]);
 
+  const { progress } = useProgress();
+
+  const BARS = 12;
+  const RADIUS = 30;
+  const WIDTH = 25;
+
+  useGSAP(() => {
+    BarRef.current.forEach((el, i) => {
+      const Prog = i / BARS;
+      const Active = progress > Prog * 100;
+      gsap.to(el, {
+        opacity: loaded ? 0 : Active ? 1 : 0.3,
+      });
+    });
+
+    return () => {};
+  }, [progress, loaded]);
+
   return (
     <div>
+      {
+        <div className="progres-bar">
+          {new Array(BARS).fill("_").map((_, i) => {
+            const ThetaPerBar = (2 * Math.PI) / BARS;
+            const Theta = ThetaPerBar * i - Math.PI / 2;
+
+            const PX = Math.cos(Theta) * RADIUS - WIDTH / 2;
+            const PY = Math.sin(Theta) * RADIUS;
+
+            return (
+              <div
+                ref={(el) => (BarRef.current[i] = el)}
+                className="bar"
+                key={i}
+                style={{
+                  opacity: 0.3,
+                  width: WIDTH,
+                  position: "absolute",
+                  transform: `translate(${PX}px,${PY}px) rotate(${Theta}rad)`,
+                }}
+              ></div>
+            );
+          })}
+        </div>
+      }
       <div ref={TopRef} className="overlay top-overlay"></div>
       <div ref={BottomRef} className="overlay bottom-overlay"></div>
     </div>
